856fc375f21234bb77fda2349d9d576ff8ec722c
MontyNum <numpymonty@yandex.ru>
2016-12-25 02:04:47 +0300

    Update pprint_json.py

--- pprint_json.py
+++ pprint_json.py
@@ -7,16 +7,20 @@ def load_data(json_file_path):
     if not os.path.exists(json_file_path):
         print('There is no such path or file. Exiting...')
         sys.exit()
-    with open(json_file_path, 'rb') as file_handler:
-        encoding = chardet.detect(file_handler.readline())['encoding']
+    encoding = detect_encoding(json_file_path)
     with open(json_file_path, 'r', encoding=encoding) as file_handler:
         return json.load(file_handler)
+
+def detect_encoding(file_path):
+    with open(file_path, 'rb') as file_handler:
+        encoding = chardet.detect(file_handler.readline())
+        return encoding['encoding']
               
 def pretty_print_json(json_content):
-    print(json.dumps(json_content, indent=4, sort_keys=True, ensure_ascii=False))
+    return json.dumps(json_content, indent=4, sort_keys=True, ensure_ascii=False)
 
     
 if __name__ == '__main__':
     file_path = input('Enter file path: ')
     json_content = load_data(file_path)
-    pretty_print_json(json_content)
+    print(pretty_print_json(json_content))
